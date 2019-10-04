9c7f3be4d6deb600b76bdcc726004badd3408279
Ridha Mezrigui <47607249+ridhamz@users.noreply.github.com>
2019-10-04 20:30:03 +0100

    Updates

---
+++ precache-manifest.1e2f5e94c1520ab9069feb197958f031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043c0ed5a41a7096a50fc53f08a0099c",
+    "url": "/React-Shopping-Cart/index.html"
+  },
+  {
+    "revision": "39ac55a69fa9051934ad",
+    "url": "/React-Shopping-Cart/static/css/main.74e8f255.chunk.css"
+  },
+  {
+    "revision": "7af01b29552cd64f2ad3",
+    "url": "/React-Shopping-Cart/static/js/2.3825175f.chunk.js"
+  },
+  {
+    "revision": "39ac55a69fa9051934ad",
+    "url": "/React-Shopping-Cart/static/js/main.c1c831b5.chunk.js"
+  },
+  {
+    "revision": "61a683a763201f1b8c82",
+    "url": "/React-Shopping-Cart/static/js/runtime-main.075dcfe2.js"
+  }
+]);
